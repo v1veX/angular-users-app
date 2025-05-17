18bcd8adeb8c004cd6c52eb9ec220a78554bbd2a
v1veX <sakmarkin.va@yandex.ru>
2025-05-17 19:05:21 +1000

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { UsersListComponent } from './users-list/users-list.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { CreateUserComponent } from './create-user/create-user.component';
+import { EditUserComponent } from './edit-user/edit-user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: UsersListComponent,
+        title: 'Users',
+    },
+    {
+        path: 'user/:id',
+        component: UserDetailsComponent,
+        title: 'User details',
+    },
+    {
+        path: 'create-user',
+        component: CreateUserComponent,
+        title: 'Create user',
+    },
+    {
+        path: 'edit-user/:id',
+        component: EditUserComponent,
+        title: 'Edit user',
+    },
+];

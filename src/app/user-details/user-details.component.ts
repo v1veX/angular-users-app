ea6bfb7447f982c8746387b181c570c1e8491aab
v1veX <sakmarkin.va@yandex.ru>
2025-05-18 20:02:43 +1000

    add details component markup and style

--- src/app/user-details/user-details.component.ts
+++ src/app/user-details/user-details.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-user-details',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './user-details.component.html',
   styleUrl: './user-details.component.scss'
 })

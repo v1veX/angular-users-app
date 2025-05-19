9fe525c213d0b9b6870eb3096143e3dba0ec94d0
v1veX <sakmarkin.va@yandex.ru>
2025-05-19 17:34:29 +1000

    update details for data dispalying

--- src/app/user-details/user-details.component.ts
+++ src/app/user-details/user-details.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { RouterLink, ActivatedRoute } from '@angular/router';
+import { UsersService } from '../shared/services/users.service';
+import { User } from '../shared/interfaces/user';
 
 @Component({
   selector: 'app-user-details',
@@ -8,5 +10,31 @@ import { RouterLink } from '@angular/router';
   styleUrl: './user-details.component.scss'
 })
 export class UserDetailsComponent {
+  private usersService = inject(UsersService);
+  private route = inject(ActivatedRoute);
+  user: User | undefined;
+  deletionInProgress = false;
 
+  constructor() {
+    const userId = Number(this.route.snapshot.params['id']);
+    this.usersService.getUser(userId).subscribe(user => {
+      this.user = user;
+    });
+  }
+
+  onDeleteClick() {
+    const userId = this.user?.id;
+
+    if (!userId) {
+      return;
+    }
+
+    this.deletionInProgress = true;
+    this.usersService.deleteUser(userId).subscribe(res => {
+      if (res.ok) {
+        console.log(`User with id ${userId} is deleted.\nServer response status: ${res.status}`);
+        this.deletionInProgress = false;
+      }
+    });
+  }
 }

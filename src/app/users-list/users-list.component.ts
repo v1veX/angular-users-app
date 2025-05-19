765976a69aed6234d05188f420a613771797f7f2
v1veX <sakmarkin.va@yandex.ru>
2025-05-19 17:34:44 +1000

    update users-list for data dispalying

--- src/app/users-list/users-list.component.ts
+++ src/app/users-list/users-list.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { UsersService } from '../shared/services/users.service';
+import { User } from '../shared/interfaces/user';
 
 @Component({
   selector: 'app-users-list',
-  imports: [RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './users-list.component.html',
   styleUrl: './users-list.component.scss'
 })
 export class UsersListComponent {
-  
+  private usersService = inject(UsersService);
+  users: User[] | undefined;
+
+  constructor() {
+    this.usersService.getAllUsers().subscribe(users => {
+      this.users = users;
+    })
+  }
 }

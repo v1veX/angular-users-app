863d949e7a177cc1d17b839349ade11005fa2922
v1veX <sakmarkin.va@yandex.ru>
2025-05-20 14:41:48 +1000

    update create-user component

--- src/app/create-user/create-user.component.ts
+++ src/app/create-user/create-user.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { UserFormComponent } from '../user-form/user-form.component';
+import { User } from '../shared/interfaces/user';
 
 @Component({
   selector: 'app-create-user',
-  imports: [],
+  imports: [RouterLink, UserFormComponent],
   templateUrl: './create-user.component.html',
   styleUrl: './create-user.component.scss'
 })
 export class CreateUserComponent {
-
+  user: User = {
+    id: 20,
+    name: 'sds',
+    username: 'sdsd',
+    email: 'sdsd',
+    address: {
+      city: 'sds',
+      street: 'dsfsdf',
+      suite: 'dsfsf',
+      zipcode: 'sdsd'
+    },
+    phone: 'dsfsf',
+    website: 'dsfsdf',
+    company: {
+      name: 'dsfsf'
+    }
+  }
 }
